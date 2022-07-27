9519736d8e9bbba92d5b4e7ab025e85c36c57a1f
Mohammad <mohammad.hijazi.2000@gmailcom>
2022-07-27 19:37:01 +0300

    edits

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className="logo">
-        <Link href="/">Mhd Headphones</Link>
+        <Link href="/">Mhd-Headphones</Link>
       </p>
 
       <button
